fcacf7a69b9c4f5ab647b837a46f0ce6817b63c5
Ryan Neph <neph320@gmail.com>
2022-03-22 23:14:19 -0700

    project_euler_001 solved

--- asm_static_library/main.cpp
+++ asm_static_library/main.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
 
 // Assembly Function Declarations
-extern "C" void asm_print_hello();
+extern "C" void asm_print_hello(void);
+extern "C" int asm_signed_multiply(int, int);
+extern "C" uint64_t asm_euler001_sum_of_multiples(uint64_t);
 
 int main (int argc, char *argv[]) {
    asm_print_hello();
+
+   const int a = 3;
+   const int b = 5;
+   int c = asm_signed_multiply(a, b);
+   printf("%d * %d = %d\n\n", a, b, c);
+
+   uint64_t loop_count = 1000;
+   uint64_t result = asm_euler001_sum_of_multiples(loop_count);
+   printf("001. sum of multiples of 3 and 5: %ld\n", result);
 }
