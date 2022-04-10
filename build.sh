93f6a85765adae717377ac401bbce60244853771
Ryan Neph <neph320@gmail.com>
2022-04-09 20:27:18 -0700

    build.sh: indicate success

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ mkdir -p "$OUT"
 # assemble library
 nasm -f elf64 -g -F dwarf -O0 -wall \
   -o $OUT/libasm.a  lib.asm
+echo -e "\"libasm.a\" build succeeded."
 
 # build executable + link library
 $CXX -g -Wall -Werror -static \
   -o $OUT/main  main.cpp $OUT/libasm.a
+echo -e "\"main\" build succeeded."
